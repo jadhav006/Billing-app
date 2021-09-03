728d9664cef731a26a7939a23f23d5013f457b63
Sumanth-MS <sumanthmanjunatha.ms@gmail.com>
2021-09-03 14:57:56 +0530

    first commit

---
+++ src/components/home/homePage/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import WelcomePage from './WelcomePage'
+import { Grid } from '@material-ui/core'
+
+const HomePage = () => {
+	return (
+		<Grid>
+			<WelcomePage />
+		</Grid>
+	)
+}
+
+export default HomePage
+
+
